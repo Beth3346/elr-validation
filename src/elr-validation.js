7cf9cfcbed4383324019fd4011e4ad9df927985f
Elizabeth Rogers <beth3346@gmail.com>
2015-06-07 16:09:01 -0500

    updated js

---
+++ src/elr-validation.js
@@ -0,0 +1,12 @@
+(function($) {
+    window.elrValidation = function(params) {
+        var self = {},
+            spec = params || {};
+
+        if ( 9 === 10 ) {
+            console.log(spec, $);
+        }
+
+        return self;
+    };
+})(jQuery);
